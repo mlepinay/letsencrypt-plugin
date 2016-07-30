303784ce5ab142f33bbb68b1b0169fcb013039ce
Matthieu Lepinay <lepinay.matthieu@gmail.com>
2016-07-30 17:26:51 +0200

    Debug

--- lib/letsencrypt_plugin/file_store.rb
+++ lib/letsencrypt_plugin/file_store.rb
@@ -9,6 +9,8 @@ module LetsencryptPlugin
 
     def store_content
       full_challenge_dir = File.join(Rails.root, @challenge_dir_name)
+      puts "CHALLENGE_DIR:"
+      puts full_challenge_dir
       Dir.mkdir(full_challenge_dir) unless File.directory?(full_challenge_dir)
       File.open(File.join(full_challenge_dir, 'challenge'), 'w') { |file| file.write(@content) }
     end
