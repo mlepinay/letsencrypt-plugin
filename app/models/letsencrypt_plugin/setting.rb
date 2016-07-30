1fb6471cb7b31a23e15d8cd0b02c35fe27fcd780
Matthieu Lepinay <lepinay.matthieu@gmail.com>
2016-07-30 16:37:19 +0200

    Update setting.rb to work without activerecord

--- app/models/letsencrypt_plugin/setting.rb
+++ app/models/letsencrypt_plugin/setting.rb
@@ -1,4 +1,9 @@
 module LetsencryptPlugin
-  class Setting < ActiveRecord::Base
+  if LetsencryptPlugin.config.challenge_dir_name.blank? && defined?(ActiveRecord::Base) == 'constant' && ActiveRecord::Base.class == Class
+    class Setting < ActiveRecord::Base
+    end
+  else
+    class Setting
+    end
   end
 end
